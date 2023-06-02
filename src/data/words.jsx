f2ee4e9f869d7e51feef39a5cfeaa3137d1f3405
TalysonSoares <talysonsoares40@gmail.com>
2023-06-02 14:30:51 -0300

    criando objeto com lista de palavras

---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordslist = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Kiwi", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "Javascript", "React"],
+    time: ["Barcelona", "Liverpool", "Roma", "Chelsea"] 
+}
