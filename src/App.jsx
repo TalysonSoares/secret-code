bc829ae3a98dbbe37db2c00a4f29c25c04de8165
TalysonSoares <talysonsoares40@gmail.com>
2023-06-02 17:44:21 -0300

    criando funções para transitar entre os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -23,15 +23,26 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  const startGame = () =>{
-    setGameStage(stages[1].name)
+  // starts the secret code game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  // restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
   }
 
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame}/>}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   )
 }
