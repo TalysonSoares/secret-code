d262dda60906d5c2a1c176d6ec050f52bb44f535
TalysonSoares <talysonsoares40@gmail.com>
2023-06-04 00:05:58 -0300

    escolhendo a palavra

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,16 @@ function App() {
   const [letters, setLetters] = useState([]);
 
   const pickWordAndCategory = () => {
+    //pick a random category
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
-    console.log(category)
+    console.log(category);
+
+    //pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    console.log(word);
+
+    return [word, category]
 
   }
   
