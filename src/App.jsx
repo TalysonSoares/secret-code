e957c53ef8c41409a205d7f342890b157c2da50b
TalysonSoares <talysonsoares40@gmail.com>
2023-06-04 22:10:38 -0300

    consumindo states do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,11 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
   const pickWordAndCategory = () => {
     //pick a random category
     const categories = Object.keys(words);
@@ -74,7 +79,14 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame}/>}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} 
+      pickedWord={pickedWord} 
+      pickedCategory={pickedCategory} 
+      letters={letters} 
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters} 
+      guesses={guesses} 
+      score={score}/>}
       {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   )
