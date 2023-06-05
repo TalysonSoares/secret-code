f69883256acbac6f59008a4a52ec232f9c9951ca
TalysonSoares <talysonsoares40@gmail.com>
2023-06-05 10:05:52 -0300

    colocando o foco no input de forma automática

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,8 @@ function App() {
   }
 
   // process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter)
   }
 
   // restarts the game
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import "./Game.css"
 
 const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score }) => {
-  const [letter, setLetter] = useState("")
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null)
+  
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter("");
+
+    letterInputRef.current.focus();
+  }
+
   return (
     <div className="game">
       <p className="points">
@@ -14,16 +26,16 @@ const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetter
       <div className="wordContainer">
         {letters.map((letter, i) => (
           guessedLetters.includes(letter) ? (
-            <span className="letter">{letter}</span>
+            <span key={i} className="letter">{letter}</span>
           ) : (
-            <span className="blankSquare"></span>
+            <span key={i} className="blankSquare"></span>
           )
         ))}
       </div>
       <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
-        <form>
-          <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)}/>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)} value={letter} ref={letterInputRef}/>
           <button>Jogar!</button>
         </form>
       </div>
