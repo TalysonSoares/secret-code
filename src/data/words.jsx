d467044d435198eba75e3fc57d45cbfa78679925
TalysonSoares <talysonsoares40@gmail.com>
2023-06-06 16:48:41 -0300

    adicionando mais palavras

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -1,8 +1,10 @@
 export const wordsList = {
     carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-    fruta: ["Banana", "Maçã", "Kiwi", "Laranja"],
+    fruta: ["Banana", "Maçã", "Kiwi", "Laranja", "Abacaxi", "Morango", "Melancia", "Pêssego", "Cereja", "Limão", "Mirtilo"],
     corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
     computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-    programação: ["Linguagem", "Framework", "Javascript", "React"],
-    time: ["Barcelona", "Liverpool", "Roma", "Chelsea"] 
+    programação: ["Linguagem", "Framework", "Javascript", "React", "Git", "Algoritmo", "API", "Variável"],
+    time: ["Barcelona", "Liverpool", "Roma", "Chelsea", "Juventus", "Milan", "Inter", "Santos", "Vasco", "Palmeiras"],
+    países: ["Alemanha", "China", "Canadá", "Japão", "Egito", "México", "Argentina", "Espanha", "índia", "Grécia"],
+    animais: ["leão", "girafa", "elefante", "canguru", ]
 }
