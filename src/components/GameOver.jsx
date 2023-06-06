f712ab11b85165c8d1820584275030a0a01271cc
TalysonSoares <talysonsoares40@gmail.com>
2023-06-06 20:15:29 -0300

    usando lottie para adicionar animações

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,11 +1,21 @@
 import "./GameOver.css"
+import Lottie from "lottie-react";
+import astronaut from "./astronaut.json";
 
 const GameOver = ({ retry, score }) => {
+  const options = {
+    animationData: astronaut,
+    loop: true
+  };
+
   return (
     <div>
       <h1>Game Over</h1>
       <h2>A sua pontuação foi: <span>{score}</span></h2>
       <button onClick={retry}>Jogar novamente</button>
+      <div className="astronaut">
+        <Lottie animationData={astronaut} loop={true} />
+      </div>
     </div>
   )
 }
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,10 +1,27 @@
-import "./StartScreen.css"
+import "./StartScreen.css";
+import { useLottie } from "lottie-react";
+import logoReact from "./logoReact.json";
 
 const StartScreen = ({ startGame }) => {
+    const options = {
+      animationData: logoReact,
+      loop: true
+    };
+
+    const style = {
+      heigth: 300,
+      width: 300
+    }
+
+    const { View } = useLottie(options, style);
+
   return <div className="start">
         <h1>Secret Code</h1>
         <p>Clique no botão abaixo para começar a jogar</p>
         <button onClick={startGame}>Começar o jogo</button>
+        <div className="logo">
+         {View}
+        </div>
     </div>
 }
 
