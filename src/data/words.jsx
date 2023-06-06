6e43813a495488de45749b0128bcc18be629d927
TalysonSoares <talysonsoares40@gmail.com>
2023-06-06 16:49:10 -0300

    adicionando mais palavras

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -1,10 +1,10 @@
 export const wordsList = {
-    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Roda", "Volante", "Banco", "Freio", "Radiador", "Escapamento"],
     fruta: ["Banana", "Maçã", "Kiwi", "Laranja", "Abacaxi", "Morango", "Melancia", "Pêssego", "Cereja", "Limão", "Mirtilo"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Nariz", "Boca", "Cotovelo", "Dedo", "Barriga",],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Cooler", "Software", "Backup", "Firewall", "Processador"],
     programação: ["Linguagem", "Framework", "Javascript", "React", "Git", "Algoritmo", "API", "Variável"],
     time: ["Barcelona", "Liverpool", "Roma", "Chelsea", "Juventus", "Milan", "Inter", "Santos", "Vasco", "Palmeiras"],
     países: ["Alemanha", "China", "Canadá", "Japão", "Egito", "México", "Argentina", "Espanha", "índia", "Grécia"],
-    animais: ["leão", "girafa", "elefante", "canguru", ]
+    animais: ["leão", "girafa", "elefante", "canguru", "tigre", "zebra", "cachorro", "urso", "lobo", "baleia", "coala"]
 }
